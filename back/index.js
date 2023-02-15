4c6c41b39bb54822eaf224831ae3b42e4b3bb8f6
danilodiaz <diazdanilo340@gmail.com>
2023-02-14 19:57:59 -0500

    commit

--- back/index.js
+++ back/index.js
@@ -12,7 +12,7 @@ const connectionOptions = {useUnifiedTopology: true, useNewUrlParser: true};
 app.use(express.json());
 app.use(cors());
 
-mongoose.connect("mongodb+srv://manuelricardo:castellanos@cluster-todolist.vhnrpzx.mongodb.net/test", connectionOptions)
+mongoose.connect("mongodb+srv://DizaDev:1601@cluster-todolist.vhnrpzx.mongodb.net/test", connectionOptions)
 //mongoose.connect("mongodb://localhost:27017/To-Do-List-Project", connectionOptions)
     .then(() => console.log("The Conection is OK"))
     .catch((err) => console.error(err));
